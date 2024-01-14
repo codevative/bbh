57a33bcda9cca80c06eda57a71d1d959c5a3a476
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-01-14 20:26:18 +0500

    feat: unit type doctype to define multiple unit types

---
+++ bbh/bbh_property_management/doctype/unit_type/unit_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Codevative and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Unit Type', {
+	// refresh: function(frm) {
+
+	// }
+});
