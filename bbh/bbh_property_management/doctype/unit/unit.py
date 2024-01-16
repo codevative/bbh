5413a209934b178dc25663ab56c2fda2836f9e02
maveecode <ameer@codevative.com>
2024-01-16 16:29:49 +0500

    feat: Item creation behind the creation of units

--- bbh/bbh_property_management/doctype/unit/unit.py
+++ bbh/bbh_property_management/doctype/unit/unit.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2024, Codevative and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Unit(Document):
-	pass
+	def after_insert(self):
+		item_data = {
+			'doctype': 'Item',
+			'item_name': self.name,
+			'item_code': self.name,
+			'description': f'Item for Unit {self.name}, Unit type: {self.unit_type}, Area: {self.unit_size_sq_m}',
+			'item_group': 'Services',
+		}
+
+		item = frappe.get_doc(item_data)
+		item.insert()
+
+		# Update the linked_item field in the Unit document
+		frappe.db.set_value('Unit', self.name, 'linked_item', item.name)
