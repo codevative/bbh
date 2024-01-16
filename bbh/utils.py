de240511d62216f28b0a193ba82344b81c5010be
maveecode <ameer@codevative.com>
2024-01-16 15:03:06 +0500

    fix: resolved naming error in unit creation when there were units previously present as well

--- bbh/utils.py
+++ bbh/utils.py
@@ -6,7 +6,11 @@ def create_units_for_floor(floor, data):
     floor_doc = frappe.get_doc('Floor', floor)
     data = json.loads(data)
     units = data.get("num_units")
-    for i in range(units):
+    previous_high = 0
+    if frappe.db.exists("Unit", {"floor_no": floor_doc.name}):
+        previous_high = frappe.db.get_value("Unit", {"floor_no": floor_doc.name}, "unit_no", order_by="unit_no DESC")
+
+    for i in range(int(previous_high), int(previous_high)+int(units)):
         unit = frappe.get_doc({
             "doctype": "Unit",
             "project": floor_doc.project,
