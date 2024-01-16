fab2a0ad1f337fa5875ecec6c34f18f5152def34
maveecode <ameer@codevative.com>
2024-01-16 18:45:23 +0500

    feat: creation and updation of price of item against the unit

--- bbh/bbh_property_management/doctype/unit/unit.py
+++ bbh/bbh_property_management/doctype/unit/unit.py
@@ -19,3 +19,28 @@ class Unit(Document):
 
 		# Update the linked_item field in the Unit document
 		frappe.db.set_value('Unit', self.name, 'linked_item', item.name)
+		self.create_or_update_item_price()
+
+	def on_update(self):
+		# Check if the price has changed and update the item price
+		if self.has_value_changed('price'):
+			self.create_or_update_item_price()
+
+
+	def create_or_update_item_price(self):
+			existing_price = frappe.db.get_value('Item Price', {'item_code': self.name}, 'name')
+
+			item_price_data = {
+				'doctype': 'Item Price',
+				'price_list': 'Standard Selling',
+				'item_code': self.name,
+				'price_list_rate': self.price
+			}
+
+			if existing_price:
+				item_price = frappe.get_doc('Item Price', existing_price)
+				item_price.update(item_price_data)
+				item_price.save()
+			else:
+				item_price = frappe.get_doc(item_price_data)
+				item_price.insert()
