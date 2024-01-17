ca09110af3e2e4f9e890c9a627ff285dd07f7766
maveecode <ameer@codevative.com>
2024-01-17 16:12:00 +0500

    feat: Overriding sales invoice for better user experience in selecting the item/unit to sale

---
+++ bbh/public/js/custom_sales_invoice.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2024, Codevative and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Invoice', {
+    refresh(frm) {
+        frm.set_query(
+            "custom_building",
+            "items",
+            function (frm, cdt, cdn) {
+                return {
+                    filters: {
+                        project: frm.custom_project
+                    },
+                };
+            }
+        );
+
+        frm.set_query(
+            'custom_floor',
+            'items',
+            function (doc, cdt, cdn) {
+                let d = locals[cdt][cdn];
+                return {
+                    filters: {
+                        project: frm.custom_project,
+                        building: d.custom_building,
+                    }
+                };
+            }
+        );
+
+        frm.set_query(
+            'custom_unit',
+            'items',
+            function (doc, cdt, cdn) {
+                let d = locals[cdt][cdn];
+                return {
+                    filters: {
+                        project: frm.custom_project,
+                        building: d.custom_building,
+                        floor_no: d.custom_floor,
+                        linked_item: ['!=', ''],
+                    }
+                };
+            }
+        );
+    },
+
+    custom_project: function (frm) {
+        frm.set_query(
+            "custom_building",
+            "items",
+            function (frm, cdt, cdn) {
+                return {
+                    filters: {
+                        project: frm.custom_project,
+                    },
+                };
+            }
+        );
+    },
+});
