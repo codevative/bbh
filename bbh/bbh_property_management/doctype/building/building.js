250151e848d0a5b71961767a7c64c5d8bd1f96fa
maveecode <ameer@codevative.com>
2024-01-18 17:18:11 +0500

    feat: client side for creating the button for floor creation on a building

--- bbh/bbh_property_management/doctype/building/building.js
+++ bbh/bbh_property_management/doctype/building/building.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Building', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		frm.add_custom_button(__('Create Floors'), function () {
+			// Create a new Dialog
+			let dialog = new frappe.ui.Dialog({
+				title: __("Create Floors"),
+				fields: [
+					{ fieldname: 'no_of_floors', label: __('Number of Floors'), fieldtype: 'Int' },
+					{ fieldname: 'col_brk', fieldtype: 'Column Break' },
+					{ fieldname: 'total_area', label: __('Total Area Sq. M'), fieldtype: 'Float' },
+				],
+				primary_action_label: __('Create'),
+				primary_action(values) {
+					frappe.call({
+						method: 'bbh.utils.create_floors_for_buildings',
+						args: { building: frm.doc.name, data: values },
+						callback: function (r) {
+							// Handle response
+							dialog.hide();
+						}
+					});
+				}
+			});
 
-	// }
+			dialog.show();
+		});
+	}
 });
