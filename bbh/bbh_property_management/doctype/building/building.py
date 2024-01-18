434b70eb21f32ec0dd6b50067544fb98c72f6978
maveecode <ameer@codevative.com>
2024-01-18 17:19:15 +0500

    fix: commenting the code of floor creation in building doc

--- bbh/bbh_property_management/doctype/building/building.py
+++ bbh/bbh_property_management/doctype/building/building.py
@@ -5,20 +5,21 @@ import frappe
 from frappe.model.document import Document
 
 class Building(Document):
-	def after_insert(self):
-		if self.has_floors and not self.no_of_floors:
-			frappe.throw("No. of Floors is mandatory when Has Floors is checked")
-		if not self.has_floors and self.no_of_floors:
-			self.no_of_floors = 0
-		if self.has_floors and self.no_of_floors:
-			for i in range(self.no_of_floors):
-				floor = frappe.get_doc({
-					"doctype": "Floor",
-					"building": self.name,
-					"project": self.project,
-					"floor_number": int(i+1),
-					"floor_name": "Floor "+str(i+1),
-					"total_area": self.covered_area
-				})
-				floor.insert()
+    pass
+	# def after_insert(self):
+	# 	if self.has_floors and not self.no_of_floors:
+	# 		frappe.throw("No. of Floors is mandatory when Has Floors is checked")
+	# 	if not self.has_floors and self.no_of_floors:
+	# 		self.no_of_floors = 0
+	# 	if self.has_floors and self.no_of_floors:
+	# 		for i in range(self.no_of_floors):
+	# 			floor = frappe.get_doc({
+	# 				"doctype": "Floor",
+	# 				"building": self.name,
+	# 				"project": self.project,
+	# 				"floor_number": int(i+1),
+	# 				"floor_name": "Floor "+str(i+1),
+	# 				"total_area": self.covered_area
+	# 			})
+	# 			floor.insert()
 
