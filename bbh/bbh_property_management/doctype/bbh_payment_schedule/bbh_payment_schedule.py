be23172a374479994815472fc9bcf618a02e0470
maveecode <ameer@codevative.com>
2024-01-22 21:46:35 +0500

    feat: payment childtable but needs to be deleted

---
+++ bbh/bbh_property_management/doctype/bbh_payment_schedule/bbh_payment_schedule.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Codevative and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BBHPaymentSchedule(Document):
+	pass
