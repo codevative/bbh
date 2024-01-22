fe40e736db1ff54553abab57246b6d5b93b20b53
maveecode <ameer@codevative.com>
2024-01-22 21:46:00 +0500

    js functions/buttons for creating payments

--- bbh/public/js/custom_sales_invoice.js
+++ bbh/public/js/custom_sales_invoice.js
@@ -47,6 +47,65 @@ frappe.ui.form.on('Sales Invoice', {
                 };
             }
         );
+
+        frm.add_custom_button(__('Create Payment Schedule'), function () {
+            let dialog = new frappe.ui.Dialog({
+                title: __("Create Payment Schedule"),
+                fields: [
+                    {
+                        fieldname: 'child_table',
+                        fieldtype: 'Table',
+                        label: 'Payment Schedule',
+                        fields: [
+                            {
+                                fieldtype: 'Int',
+                                fieldname: 'payment_no',
+                                label: 'Payment No',
+                                in_list_view: 1
+                            },
+                            {
+                                fieldtype: 'Date',
+                                fieldname: 'payment_date',
+                                label: 'Payment Date',
+                                in_list_view: 1
+                            },
+                            {
+                                fieldtype: 'Currency',
+                                fieldname: 'payment_amount',
+                                label: 'Payment Amount',
+                                in_list_view: 1
+                            },
+                        ],
+                        data: [],
+                        get_data: function() {
+                            return this.data;
+                        }
+                    }
+                ],
+                primary_action_label: __('Create'),
+                primary_action(values) {
+                    frappe.call({
+						method: 'bbh.utils.create_terms',
+                        args: { invoice: frm.doc.name, data: values },
+                        callback: function (r) {
+                            if(r.message) {
+                                // Update the payment_terms_template field
+                                console.log(r.message);
+                                frm.set_value('payment_terms_template', r.message);
+
+                                // Optionally, refresh the payment_schedule to reflect the changes
+                                frm.refresh_field('payment_schedule');
+
+                                dialog.hide();
+
+                            }
+                        }
+                    });
+                }
+            });
+
+            dialog.show();
+        });
     },
 
     project: function (frm) {
