e47200e7fae5ce733870f403f8b701f16867afcd
maveecode <ameer@codevative.com>
2024-01-23 12:36:51 +0500

    feat: show create units button only when the doc is saved

--- bbh/bbh_property_management/doctype/floor/floor.js
+++ bbh/bbh_property_management/doctype/floor/floor.js
@@ -4,31 +4,31 @@
 frappe.ui.form.on('Floor', {
 
 	refresh(frm) {
-		frm.add_custom_button(__('Create Units'), function () {
-			// Create a new Dialog
-			let dialog = new frappe.ui.Dialog({
-				title: __("Create Units"),
-				fields: [
-					{ fieldname: 'unit_type', label: __('Unit Type'), fieldtype: 'Link', options: 'Unit Type' },
-					{ fieldname: 'num_units', label: __('Number of Units'), fieldtype: 'Int' },
-					{ fieldname: 'col_brk', fieldtype: 'Column Break' },
-					{ fieldname: 'unit_size_sq_m', label: __('Unit Size Sq. M'), fieldtype: 'Float' },
-					{ fieldname: 'price', label: __('Price'), fieldtype: 'Currency' },
-				],
-				primary_action_label: __('Create'),
-				primary_action(values) {
-					frappe.call({
-						method: 'bbh.utils.create_units_for_floor',
-						args: { floor: frm.doc.name, data: values },
-						callback: function (r) {
-							// Handle response
-							dialog.hide();
-						}
-					});
-				}
-			});
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Create Units'), function () {
+				let dialog = new frappe.ui.Dialog({
+					title: __("Create Units"),
+					fields: [
+						{ fieldname: 'unit_type', label: __('Unit Type'), fieldtype: 'Link', options: 'Unit Type' },
+						{ fieldname: 'num_units', label: __('Number of Units'), fieldtype: 'Int' },
+						{ fieldname: 'col_brk', fieldtype: 'Column Break' },
+						{ fieldname: 'unit_size_sq_m', label: __('Unit Size Sq. M'), fieldtype: 'Float' },
+						{ fieldname: 'price', label: __('Price'), fieldtype: 'Currency' },
+					],
+					primary_action_label: __('Create'),
+					primary_action(values) {
+						frappe.call({
+							method: 'bbh.utils.create_units_for_floor',
+							args: { floor: frm.doc.name, data: values },
+							callback: function (r) {
+								dialog.hide();
+							}
+						});
+					}
+				});
 
-			dialog.show();
-		});
+				dialog.show();
+			});
+		}
 	}
 });
