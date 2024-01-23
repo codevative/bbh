2046c2ed6a88d77fa09f154e349277717ca0cc82
maveecode <ameer@codevative.com>
2024-01-23 12:36:06 +0500

    feat: show create floor buttons only when the doc is saved

--- bbh/bbh_property_management/doctype/building/building.js
+++ bbh/bbh_property_management/doctype/building/building.js
@@ -3,29 +3,29 @@
 
 frappe.ui.form.on('Building', {
 	refresh(frm) {
-		frm.add_custom_button(__('Create Floors'), function () {
-			// Create a new Dialog
-			let dialog = new frappe.ui.Dialog({
-				title: __("Create Floors"),
-				fields: [
-					{ fieldname: 'no_of_floors', label: __('Number of Floors'), fieldtype: 'Int' },
-					{ fieldname: 'col_brk', fieldtype: 'Column Break' },
-					{ fieldname: 'total_area', label: __('Total Area Sq. M'), fieldtype: 'Float' },
-				],
-				primary_action_label: __('Create'),
-				primary_action(values) {
-					frappe.call({
-						method: 'bbh.utils.create_floors_for_buildings',
-						args: { building: frm.doc.name, data: values },
-						callback: function (r) {
-							// Handle response
-							dialog.hide();
-						}
-					});
-				}
-			});
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Create Floors'), function () {
+				let dialog = new frappe.ui.Dialog({
+					title: __("Create Floors"),
+					fields: [
+						{ fieldname: 'no_of_floors', label: __('Number of Floors'), fieldtype: 'Int' },
+						{ fieldname: 'col_brk', fieldtype: 'Column Break' },
+						{ fieldname: 'total_area', label: __('Total Area Sq. M'), fieldtype: 'Float' },
+					],
+					primary_action_label: __('Create'),
+					primary_action(values) {
+						frappe.call({
+							method: 'bbh.utils.create_floors_for_buildings',
+							args: { building: frm.doc.name, data: values },
+							callback: function (r) {
+								dialog.hide();
+							}
+						});
+					}
+				});
 
-			dialog.show();
-		});
+				dialog.show();
+			});
+		}
 	}
 });
