12afdadade51d31e2abdf0ba016e4bda3c7b7052
mavee <mavee.shah@hotmail.com>
2024-01-30 18:08:12 +0500

    feat: changing labels of subscription plan and subscription to handle rent module

---
+++ bbh/public/js/custom_subscription_plan.js
@@ -0,0 +1,47 @@
+frappe.ui.form.on('Subscription Plan', {
+    refresh(frm) {
+
+        frm.set_query("building", function () {
+            let project = "";
+            if (frm.doc.project) {
+                project = frm.doc.project;
+            }
+            return {
+                filters: {
+                    project: project,
+                    has_floors: 1,
+                },
+            };
+        });
+
+        frm.set_query("floor", function () {
+            let building = "";
+            if (frm.doc.building) {
+                building = frm.doc.building;
+            }
+            return {
+                filters: {
+                    project: frm.doc.project,
+                    building: building,
+                    has_units: 1,
+                },
+            };
+        });
+
+        frm.set_query("custom_unit", function () {
+            let floor_no = "";
+            if (frm.doc.floor) {
+                floor_no = frm.doc.floor;
+            }
+            return {
+                filters: {
+                    floor_no: floor_no,
+                    building: frm.doc.building,
+                    project: frm.doc.project,
+                },
+            };
+        });
+
+    },
+
+});
