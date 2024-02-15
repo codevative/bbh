2a59579720af87c44fa23ef05fc64463db03e06f
mavee <mavee.shah@hotmail.com>
2024-02-15 07:21:06 +0500

    feat: rental modules

--- bbh/bbh_property_management/doctype/landlord/landlord.py
+++ bbh/bbh_property_management/doctype/landlord/landlord.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2024, Codevative and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Landlord(Document):
-	pass
+	def before_insert(self):
+		customer = frappe.get_doc({
+			'doctype': 'Customer',
+			'customer_name': self.landlord_name,
+			'custom_id': self.id,
+			'custom_email': self.email,
+			'custom_mobile': self.mobile,
+			'custom_is_landlord': 1,
+		})
+		customer.insert(ignore_permissions=True)
+		self.customer = customer.name
+		self.name = customer.name
+
+	def on_trash(self):
+		customer_name = self.db_get('customer')
+		if customer_name and frappe.db.exists('Customer', customer_name):
+			frappe.delete_doc('Customer', customer_name, ignore_permissions=True)
+
--- bbh/hooks.py
+++ bbh/hooks.py
@@ -185,6 +185,7 @@ doc_events = {
 # -----------------------------------------------------------
 
 # ignore_links_on_delete = ["Communication", "ToDo"]
+ignore_links_on_delete = ["Customer", "Tenant","Landlord"]
 
 # Request Events
 # ----------------
